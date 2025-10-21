9d108e37ad7d9505c0de8f9ae0749252763d0288
albert <alberix252@gmail.com>
2025-10-21 18:50:45 -0500

    deje funcional la seccion testimonios, tarjetas con carrusel automatico, se modificaron estilos indes.css y se dejo funcional la function del carrusel en index.js

--- assets/website_externos/index/index.js
+++ assets/website_externos/index/index.js
@@ -1,3 +1,4 @@
+// menu hamburguesa
 const toggle = document.getElementById('menu-toggle');
 const nav = document.getElementById('navbarNav');
 
@@ -15,7 +16,7 @@ $(document).ready(function () {
         arrows: true,          // Muestra flechas
         dots: true,            // Muestra los puntitos de navegación
         autoplay: true,        // Activa autoplay
-        autoplaySpeed: 3000,   // Velocidad del autoplay en ms
+        autoplaySpeed: 2500,   // Velocidad del autoplay en ms
         responsive: [
             {
                 breakpoint: 1024, // Tablets y laptops pequeñas
@@ -32,26 +33,6 @@ $(document).ready(function () {
         ]
     });
 
-});
 
 
-// flechas prev y next
-$('.slider-testimonios').slick({
-    dots: true,
-    arrows: true,
-    prevArrow: '<button type="button" class="slick-prev"><i class="fas fa-chevron-left"></i></button>',
-    nextArrow: '<button type="button" class="slick-next"><i class="fas fa-chevron-right"></i></button>',
-    infinite: true,
-    slidesToShow: 3,
-    slidesToScroll: 1,
-    responsive: [
-        {
-            breakpoint: 992,
-            settings: { slidesToShow: 2 }
-        },
-        {
-            breakpoint: 768,
-            settings: { slidesToShow: 1 }
-        }
-    ]
 });
